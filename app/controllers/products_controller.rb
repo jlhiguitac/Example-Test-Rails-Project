91e99dfaced7a28312c5d781fdb7be06a70982dd
jlhiguitac <jluis.higuita@gmail.com>
2025-04-18 21:09:56 -0500

    add sort by different caracteristics, adn more fixtures

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   def index
     # pp params[:category_id]
     @categories = Category.all.order(name: :asc).load_async
-    @products = Product.all.with_attached_photo.order(created_at: :desc).load_async
+    @products = Product.all.with_attached_photo
     if params[:category_id]
       @products = @products.where(category_id: params[:category_id])
     end
@@ -15,6 +15,8 @@ class ProductsController < ApplicationController
     if params[:query_text].present?
       @products = @products.search_full_text(params[:query_text])
     end
+    orders = Product::ORDER_BY.fetch(params[:order_by], Product::ORDER_BY["newest"])
+    @products = @products.order(orders).load_async
   end
 
   def show
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -24,6 +24,24 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select ".product", 1
     assert_select "h2", "Nintendo Switch"
   end
+  test "sort products by expensive" do
+    get products_path(order_by: "expensive")
+    assert_response :success
+    assert_select ".product", 3
+    assert_select ".products .product:first-child h2", "Macbook Pro 2015"
+  end
+  test "sort products by chepeast" do
+    get products_path(order_by: "cheapest")
+    assert_response :success
+    assert_select ".product", 3
+    assert_select ".products .product:first-child h2", "PS4 Fat"
+  end
+  test "sort products by newest" do
+    get products_path(order_by: "newest")
+    assert_response :success
+    assert_select ".product", 3
+    assert_select ".products .product:first-child h2", "PS4 Fat"
+  end
   test "render a detailed product page" do
     get product_path(products(:ps4))
 
